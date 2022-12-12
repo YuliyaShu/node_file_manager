bab45dc11f2a0c3cafc40db2350b220b6b4b77d2
Yuliya_Shulgan <juliette7373@gmail.com>
2022-12-12 12:23:55 +0100

    feat: nwd/cd

--- src/handleCommands.js
+++ src/handleCommands.js
@@ -1,16 +1,21 @@
-import { up } from "./os/up.js";
+import { up } from "./nwd/up.js";
+import { cd } from './nwd/cd.js';
 import { callWorkingDirectory } from "./utils/callWorkingDirectory.js";
 
 export const handleCommands = (rl, workingDirectory) => {
-    rl.on('line', data => {
+    rl.on('line', async data => {
         switch (data) {
             case 'up':
-                workingDirectory = up(workingDirectory, rl);
+                workingDirectory = up(workingDirectory);
+                break;
+            case data.match(/^cd/)?.input:
+                workingDirectory = await cd(workingDirectory, data);
                 break;
             default:
                 callWorkingDirectory(workingDirectory);
                 console.log(`Invalid input. Type one of these commands:
-                    up`);
+                    up
+                    cd "path_to_directory"`);
         }
     });
 } 
---
+++ src/nwd/cd.js
@@ -0,0 +1,36 @@
+import { stat } from 'node:fs/promises';
+import { callWorkingDirectory } from "../utils/callWorkingDirectory.js";
+import path from 'path';
+
+export const cd = async (workingDirectory, data) => {
+    if (!data.match(/^cd "(.*)"$/)?.input) {
+        callWorkingDirectory(workingDirectory);
+        console.log('Operation failed. Incorrect format. Should use "". Did you mean cd "path_to_directory"?');
+        return workingDirectory;
+    }
+    let pathToDirectory = data.slice(4, -1);
+    if (pathToDirectory === '') {
+        callWorkingDirectory(workingDirectory);
+        console.log('Operation failed. Insert path to directory. Did you mean cd "path_to_directory"?');
+        return workingDirectory;
+    }
+    if (!path.isAbsolute(pathToDirectory)) {
+        const absPathToDirectory = path.resolve(pathToDirectory);
+        pathToDirectory = absPathToDirectory;
+    }
+    return await stat(pathToDirectory)
+        .then(stats => {
+            if (!stats.isDirectory()) {
+                callWorkingDirectory(workingDirectory);
+                console.log('Operation failed. Can not go to the file. Insert a path to the directory');
+                return workingDirectory;
+            }
+            callWorkingDirectory(pathToDirectory);
+            return pathToDirectory;
+        })
+        .catch(() => {
+            console.log('Operation failed. No such directory.');
+            callWorkingDirectory(workingDirectory);
+            return workingDirectory;
+        });
+}
