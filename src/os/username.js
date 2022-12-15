c3568d9ecad9aa8097bab93af047fa3b609ed791
Yuliya_Shulgan <juliette7373@gmail.com>
2022-12-15 13:03:41 +0100

    feat: os/username

--- src/os/username.js
+++ src/os/username.js
@@ -1,8 +1,10 @@
 import { callWorkingDirectory } from '../utils/callWorkingDirectory.js';
+import { userInfo as userInfoOs } from 'node:os';
 
 export const username = (workingDirectory) => {
     try {
         process.chdir(workingDirectory);
+        console.log(`System user name is: ${userInfoOs().username}`)
         callWorkingDirectory(workingDirectory);
     } catch (error) {
         if (error) {
